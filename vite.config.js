28dda3bf64a9540f38d59507619b4c00f940de6a
DenisY <kalimm@bk.ru>
2023-05-26 16:13:23 +0300

    index page

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/assets/plugins/fontawesome-free/css/all.min.css',
-             'public/assets/dist/css/adminlte.min.css',
-             'public/assets/plugins/jquery/jquery.min.js',
-             'public/assets/plugins/bootstrap/js/bootstrap.bundle.min.js',
-             'public/assets/dist/js/adminlte.min.js',
-             'public/assets/dist/js/demo.js'
+            input: ['public/assets/admin/plugins/fontawesome-free/css/all.min.css',
+             'public/assets/admin/dist/css/adminlte.min.css',
+             'public/assets/admin/plugins/jquery/jquery.min.js',
+             'public/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+             'public/assets/admin/dist/js/adminlte.min.js',
+             'public/assets/admin/dist/js/demo.js',
+             'resources/css/main.css',
+             'resources/css/bootstrap.min.css',
+             'resources/css/bootstrap.min.css.map.map',
+             'resources/css/fonts.css',
+             'resources/css/footer.css',
+             'resources/css/header.css',
+             'resources/js/bootstrap.bundle.min.js',
+             'resources/js/bootstrap.bundle.min.js.map.map',
+             'resources/js/bootstrap.js',
+             'resources/js/main.js'
             ],
             refresh: true,
         }),
